07edd0b13a2825e3cab5d1c98509f7e829617514
Felipe Araujo <141876774+FelipeGA02@users.noreply.github.com>
2025-07-02 16:20:41 -0300

    feat: Configurações iniciais + Rotas

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+//import Contato from './pages/Contato';
+//import Reserva from './pages/Reserva';
+import '@fontsource/roboto';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/*<Route path="/contato" element={<Contato />} />
+      <Route path="/reserva" element={<Reserva />} />*/}
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,30 @@
+//import Header from '../components/Header';
+//import Hero from '../sections/Hero';
+//import QuemSomos from '../sections/QuemSomos';
+//import Parceiros from '../sections/Parceiros';
+//import Auditório from '../sections/Espaco';
+//import Servicos from '../sections/Servicos';
+//import ComoFunciona from '../sections/ComoFunciona';
+//import Diferenciais from '../sections/Diferenciais';
+//import CTA from '../sections/CTA';
+//import Footer from '../components/Footer';
+
+export default function Home() {
+    return (
+        <>
+            {/* <Header /> */}
+            {/* <main>
+                <Hero />
+                <QuemSomos />
+                <Parceiros />
+                <Auditório />
+                <Servicos />
+                <ComoFunciona />
+                <Diferenciais />
+                <CTA />
+            </main>
+            <Footer /> */}
+            <div>Home Page</div>
+        </>
+    );
+}
