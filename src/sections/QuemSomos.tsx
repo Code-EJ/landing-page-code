3ce7d99df0542546eccae764504589b07060c34c
tosuki <okaabe2006@gmail.com>
2025-07-05 22:46:34 -0300

    refactor: rewrite about us section with the purpose of making it more responsive than before, also change the images of the section to match the design colors of the headerbar.

--- src/sections/QuemSomos.tsx
+++ src/sections/QuemSomos.tsx
@@ -1,41 +1,55 @@
-import QuemSomosImg1 from "../assets/quem_somos_1.png";
-import QuemSomosImg2 from "../assets/quem_somos_2.png"
+import type { ReactNode } from "react"
+import IdeasSvg from "../assets/quemsomos_1.svg"
+import PeopleSvg from "../assets/quemsomos_2.svg"
 
-export type QuemSomosSectionProps = {
-    image: string;
-    text: string;
-    reversed?: boolean;
-}
-
-const SectionRow = ({ image, text, reversed }: QuemSomosSectionProps) => {
+function SectionRow({ isReversed, children, image }: {
+    isReversed?: boolean,
+    children: ReactNode,
+    image: string
+}) {
     return (
         <div className={`
-            flex flex-col items-center gap-8
-            md:flex-row md:justify-between md:gap-20
-            ${reversed ? "md:flex-row-reverse" : ""}
-        `}>
-            <img src={image} alt="Quem somos" className="w-120 h-70 object-cover rounded-lg" />
-            <p className="text-xl text-center w-lg">{text}</p>
+            w-full
+            flex items-center flex-col-reverse justify-center gap-20
+            md:${ isReversed ? "flex-row-reverse" : "flex-row" }
+s        `}>
+            <div
+                className="w-full sm:w-2xl flex items-center justify-center"
+            >
+                <p className="text-center sm:text-xl">{ children }</p>
+            </div>
+            <img
+                src={ image }
+                alt={ image } 
+                className="w-100 h-100 rounded-lg"
+            />
         </div>
-    );
+    )
 }
 
-const QuemSomosSection = () => {
+function Highlight({ children }: { children: ReactNode }) {
     return (
-        <div className="quem-somos-section-container flex flex-col items-center bg-[#F5F5F5] pt-20 pb-20 pr-8 pl-8 gap-20">
-            <h1 className="font-bold text-4xl">Quem somos?</h1>
-            <SectionRow
-                image={ QuemSomosImg1 }
-                reversed={true}
-                text="A CODE[] é a Empresa Júnior de Ciência da Computação da Dom Helder, formada por estudantes apaixonados por tecnologia e impacto social. Seu propósito é conectar academia e mercado, desenvolvendo soluções tecnológicas reais que fortalecem as habilidades técnicas e profissionais dos membros."
-            />
-            <SectionRow
-                reversed={false}
-                text="A CODE[] acredita na transformação digital e no potencial da juventude para um futuro mais eficiente e sustentável. Com uma equipe multidisciplinar, entrega mais que sistemas, desenvolve pessoas."
-                image={ QuemSomosImg2 }
-            />
-        </div>
-    );
+        <span className="text-accent font-bold">{ children }</span>
+    )
 }
 
-export default QuemSomosSection;
+function QuemSomosSection() {
+    return (
+        <div className="w-full bg-alt-bg flex flex-col justify-center items-center pt-10 gap-10">
+            <h1 className="text-4xl font-bold text-accent pb-5">Quem somos nós?</h1>
+            <SectionRow
+                image={ IdeasSvg }
+            >
+                A <Highlight>CODE[]</Highlight> é a Empresa Júnior de Ciência da Computação da Dom Helder, formada por estudantes apaixonados por tecnologia e impacto social. Seu propósito é conectar academia e mercado, desenvolvendo soluções tecnológicas reais que fortalecem as habilidades técnicas e profissionais dos membros.
+            </SectionRow>
+            <SectionRow 
+                isReversed={true}
+                image={ PeopleSvg }
+            >
+                A <Highlight>CODE[]</Highlight> acredita na transformação digital e no potencial da juventude para um futuro mais eficiente e sustentável. Com uma equipe multidisciplinar, entrega mais que sistemas, desenvolve pessoas.
+            </SectionRow>
+        </div>
+    )
+}
+
+export default QuemSomosSection
