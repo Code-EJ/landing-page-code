0b788ab47033ee0ebce4983e8b59ad60be212876
tosuki <okaabe2006@gmail.com>
2025-07-07 09:16:36 -0300

    feat: create parceiros cards prototype

--- src/sections/Parceiros.tsx
+++ src/sections/Parceiros.tsx
@@ -1,5 +1,48 @@
-export function ParceirosSection() {
+export type ParceiroProperties = {
+    name: string
+    image?: string
+}
+
+export function ParceiroCard({ name, image }: ParceiroProperties) {
     return (
-        <h1>Hello World</h1>
+        <div className="w-32 h-32 border border-black flex items-center justify-center">
+            { image ? <img src={ image } alt={ name }/> : <h1>{ name }</h1>}
+        </div>
+    )
+}
+
+export function ParceirosCards({ parceiros }: { parceiros: ParceiroProperties[] }) {
+    return (
+        <div
+            className="grid grid-cols-3 gap-5"
+        >
+            { parceiros.map(({ name, image }) => {
+                return <ParceiroCard 
+                    name={ name }
+                    image={ image }
+                />
+            })}
+        </div>
+    )
+}
+
+export default function ParceirosSection() {
+    return (
+        <div
+            className="w-full pt-10 pb-10 flex items-center flex-col gap-8"
+        >
+            <h1 className="text-4xl font-bold">Nossos Parceiros</h1>
+            <p className="text-xl w-2xl text-center">Trabalhamos ao lado de empresas inovadoras que compartilham nossa visão de transformar o futuro com tecnologia</p>
+            <ParceirosCards 
+                parceiros={[
+                    { name: "Ambev" },
+                    { name: "Dom Helder" },
+                    { name: "Localiza" },
+                    { name: "Red Bull" },
+                    { name: "Sydle" },
+                    { name: "Ubuntu" }
+                ]}
+            />
+        </div>
     )
 }
