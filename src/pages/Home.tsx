352a8590a7b76edaccc8ba64c6b4efc9adfc8298
morenojones9 <d24307@academico.domhelder.edu.br>
2025-07-09 15:55:44 -0300

    feat(TRFAT-11): implementação da seção Hero com layout responsivo, botões e carrossel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,23 @@
-//import Header from '../components/Header';
-//import Hero from '../sections/Hero';
-//import QuemSomos from '../sections/QuemSomos';
-//import Parceiros from '../sections/Parceiros';
-//import Auditório from '../sections/Espaco';
-//import Servicos from '../sections/Servicos';
-//import ComoFunciona from '../sections/ComoFunciona';
-//import Diferenciais from '../sections/Diferenciais';
-//import CTA from '../sections/CTA';
-//import Footer from '../components/Footer';
-
-import Header from "../components/Header";
+import Header from '../components/Header';
+import Hero from '../sections/Hero';
+// import QuemSomos from '../sections/QuemSomos';
+// import Parceiros from '../sections/Parceiros';
+// import Auditorio from '../sections/Espaco';
+// import Servicos from '../sections/Servicos';
+// import ComoFunciona from '../sections/ComoFunciona';
+// import Diferenciais from '../sections/Diferenciais';
+// import CTA from '../sections/CTA';
+// import Footer from '../components/Footer';
 
 export default function Home() {
-    return (
-        <>
-            <Header />
-            {/* <main>
-                <Hero />
-                <QuemSomos />
-                <Parceiros />
-                <Auditorio />
-                <Servicos />
-                <ComoFunciona />
-                <Diferenciais />
-                <CTA />
-            </main>
-            <Footer /> */}
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        {/* Outros componentes virão depois */}
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
 }
