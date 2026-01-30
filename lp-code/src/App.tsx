f456dc6d851668358a035772a972b15250b2a781
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2026-01-29 23:00:52 -0300

    Add Tailwind CSS and remove unused App.css
    Integrated Tailwind CSS by adding dependencies, configuration, and updating Vite config. Removed unused App.css and updated App.tsx and index.css to use Tailwind classes.

--- lp-code/src/App.tsx
+++ lp-code/src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from 'react'
+import { useGSAP } from '@gsap/react'
+import { gsap } from "gsap";
 
-function App() {
-  const [count, setCount] = useState(0)
+gsap.registerPlugin(useGSAP);
+
+function App() { 
+  const container = useRef<HTMLDivElement | null>(null);
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+
+  const { contextSafe } = useGSAP({ scope: container });
+
+  useGSAP(() => {
+    if (!buttonRef.current) return;
+
+    gsap.from(buttonRef.current, {
+      opacity: 0,
+      y: 30,
+      duration: 0.8,
+      ease: 'power3.out',
+    });
+  }, [])
+
+const onClickGood = contextSafe(() => {
+    if (!buttonRef.current) return
+
+    gsap.to(buttonRef.current, {
+      rotation: '+=180',
+      duration: 0.3,
+      ease: 'power2.out',
+    })
+  })
+
+  const onEnter = contextSafe((e: React.MouseEvent<HTMLButtonElement>) => {
+    gsap.to(e.currentTarget, {
+      scale: 1.25,
+      duration: 0.1,
+      ease: 'power2.out',
+    })
+  })
+
+  const onLeave = contextSafe((e: React.MouseEvent<HTMLButtonElement>) => {
+    gsap.to(e.currentTarget, {
+      scale: 1,
+      duration: 0.1,
+      ease: 'power2.out',
+      overwrite: 'auto',
+    })
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div 
+    ref={container} 
+    className="flex flex-col justify-center items-center h-screen"
+    >
+      <h1>Landing Page da Code</h1>
+      <button
+        ref={buttonRef}
+        onClick={onClickGood}
+        onMouseEnter={onEnter}
+        onMouseLeave={onLeave}
+        className="px-6 py-3 text-xl bg-purple-900 text-white rounded"
+      >
+        Click
+      </button>
+    </div>
   )
 }
-
 export default App
